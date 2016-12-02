dfa770617971e8eab9eacf6c4db75f0bd7fe0a0e
Justin Simonelli <justin.simonelli@sherwin.com>
2016-12-01 20:32:26 -0500

    Initial commit for Day 1

---
+++ src/com/sims/advent/of/code/day1/EasterBunnyTaxiCab.java
@@ -0,0 +1,19 @@
+package com.sims.advent.of.code.day1;
+
+import java.awt.Point;
+import java.util.List;
+
+import com.sims.advent.of.code.utils.InputReader;
+
+public class EasterBunnyTaxiCab
+{
+    public static void main( String[] args )
+    {
+        boolean isFacingNorthOrSouth = true;
+        boolean hasStartedMoving = false;
+        Point location = new Point( 0, 0 );
+
+        List<String> directions = InputReader.getFileInputAsList( "resources/day1/input.txt" );
+
+    }
+}
---
+++ src/com/sims/advent/of/code/utils/InputReader.java
@@ -0,0 +1,74 @@
+package com.sims.advent.of.code.utils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class InputReader
+{
+
+    /**
+     * Given the <code>filePath</code> param, load the input and return it as a
+     * string.
+     * 
+     * @param filePath
+     * @return
+     * @throws IOException
+     */
+    private static Stream<String> readInputFromFile( String filePath )
+        throws IOException
+    {
+
+        return Files.lines( Paths.get( filePath ) );
+
+    }
+
+    /**
+     * Get the Stream as a single String.
+     * 
+     * @param filePath
+     * @return
+     */
+    public static String getFileInputAsString( String filePath )
+    {
+
+        try (Stream<String> stream = readInputFromFile( filePath ))
+        {
+
+            return stream.collect( Collectors.joining() );
+
+        }
+        catch( IOException e )
+        {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the Stream as a list of String(s).
+     * 
+     * @param filePath
+     * @return
+     */
+    public static List<String> getFileInputAsList( String filePath )
+    {
+        try (Stream<String> stream = readInputFromFile( filePath ))
+        {
+
+            return stream.collect( Collectors.toList() );
+
+        }
+        catch( IOException e )
+        {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+}
