039b84a35f1fc1d27603940e5121f60dd2d28b59
Justin Simonelli <justin.simonelli@gmail.com>
2016-12-05 20:48:16 -0800

    Password cracking first go around with working solutions. Refactor later

--- src/com/sims/advent/of/code/day5/PasswordCrack.java
+++ src/com/sims/advent/of/code/day5/PasswordCrack.java
@@ -6,36 +6,94 @@ import java.security.NoSuchAlgorithmException;
 import javax.xml.bind.DatatypeConverter;
 
 public class PasswordCrack {
+
+	
 	public static void main(String[] args) {
-		String password = "ffykfhsq";
-		StringBuilder pwd = new StringBuilder();
+		System.out.println("Running...");
+		String password = null;
+		String moviePassword = null;
 		try {
-			MessageDigest md = MessageDigest.getInstance("MD5");
-			int i = 0;
-			System.out.println("Running...");
-
-			while (true) {
-				if (pwd.toString().length() == 8) {
-					break;
-				}
-
-				String tmpPassword = password + i;
-				
-				md.update(tmpPassword.getBytes());
-				byte[] digest = md.digest();
-				String hash = DatatypeConverter.printHexBinary(digest);
-				if( hash.startsWith("00000") ){
-					pwd.append(hash.charAt(5));
-				}
-				i++;
-
-			}
+			password = easyBruteForce("ffykfhsq");
+			System.out.println("Easy brute password: " + password);
+		
 			
-			System.out.println("Password is " + pwd.toString().toLowerCase());
-
+			moviePassword = movieH4x1ng("ffykfhsq");
+			System.out.println("Movie password: " + moviePassword);
 		} catch (NoSuchAlgorithmException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
+		System.out.println("passwords cracked");
+	}
+	
+	public static String easyBruteForce(String inputKey) throws NoSuchAlgorithmException{
+		StringBuilder pwd = new StringBuilder();
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		int i = 0;
+
+		while (true) {
+			if (pwd.toString().length() == 8) {
+				break;
+			}
+
+			String tmpPassword = inputKey + i;
+			
+			md.update(tmpPassword.getBytes());
+			byte[] digest = md.digest();
+			String hash = DatatypeConverter.printHexBinary(digest);
+			if( hash.startsWith("00000") ){
+				pwd.append(hash.charAt(5));
+			}
+			i++;
+
+		}
+		
+		return pwd.toString().toLowerCase();
+
+	}
+	
+	public static String movieH4x1ng(String inputKey) throws NoSuchAlgorithmException{
+		Character[] password = new Character[8];
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		StringBuilder builder = new StringBuilder();
+		int i = 0;
+		int numberOfKeysFound = 0;
+		
+		while(true){
+
+			if(numberOfKeysFound == 8){
+				break;
+			}
+			
+			String tmpPassword = inputKey + i;
+			md.update(tmpPassword.getBytes());
+			byte[] digest = md.digest();
+			String hash = DatatypeConverter.printHexBinary(digest);
+			
+			if( hash.startsWith("00000") ){
+				String position  = String.valueOf(hash.charAt(5));
+				Character key = hash.charAt(6);
+				
+				if( position.matches("[0-7]") ){
+					int pos = Integer.parseInt(position);
+					
+					if( password[pos] == null ){
+						password[pos] = key;
+						numberOfKeysFound++;
+					}
+				}
+			}
+			
+			i++;
+			
+		}
+		
+		for( Character c : password ){
+			builder.append(c);
+		}
+		
+		return builder.toString().toLowerCase();
 	}
 }
